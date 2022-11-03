23ac7d420b8dfd0dc69520be9b9714dd533b06c4
taekhun <brachiosaurus19@gmail.com>
2022-11-03 15:57:29 +0900

    DRILL#12 완성
    2019180033 이택훈

---
+++ drill/12/play_state.py
@@ -0,0 +1,79 @@
+from pico2d import *
+import game_framework
+import game_world
+
+from grass import Grass
+from boy import Boy
+from grass_2 import Grass_2
+
+boy = None
+grass = None
+grass_2 = None
+
+def handle_events():
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            game_framework.quit()
+        elif (event.type, event.key) == (SDL_KEYDOWN, SDLK_ESCAPE):
+            game_framework.quit()
+        else:
+            boy.handle_event(event)
+
+
+# 초기화
+def enter():
+    global boy, grass, grass_2
+    boy = Boy()
+    grass = Grass()
+    grass_2 = Grass_2()
+    game_world.add_object(boy, 1)
+    game_world.add_object(grass, 0)
+    game_world.add_object(grass_2, 2)
+
+
+# 종료
+def exit():
+    global boy, grass, grass_2
+    del boy
+    del grass
+    del grass_2
+
+
+def update():
+    for game_object in game_world.all_objects():
+        game_object.update()
+    # boy.update()
+
+def draw_world():
+    for game_object in game_world.all_objects():
+        game_object.draw()
+    # grass.draw()
+    # boy.draw()
+
+
+def draw():
+    clear_canvas()
+    draw_world()
+    update_canvas()
+
+
+def pause():
+    pass
+
+
+def resume():
+    pass
+
+
+
+
+def test_self():
+    import play_state
+
+    pico2d.open_canvas()
+    game_framework.run(play_state)
+    pico2d.clear_canvas()
+
+if __name__ == '__main__':
+    test_self()
